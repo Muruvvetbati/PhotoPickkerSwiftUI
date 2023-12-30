d9e5cd611d76044fc794c723374a211e4aab0faa
Muruvvetbati <batimuruvvet@gmail.com>
2023-12-30 23:28:52 +0300

    Initial Commit

---
+++ PhotoPickerSwiftUI/PhotoPickerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPickerSwiftUIApp.swift
+//  PhotoPickerSwiftUI
+//
+//  Created by Devmuruvvet on 30.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPickerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
